0bde3a13bf012862703acd7bebe96abf1fd779cc
Philippe van Amerongen <philippe.vanamerongen@gmail.com>
2019-03-25 14:27:52 +0100

    Now using the networkx library

--- gen.py
+++ gen.py
@@ -1,31 +1,32 @@
+import pickle
+import networkx as nx
 import numpy as np
-from Graph import Graph
-from Node import *
 
 num_nodes = 1000
 num_sectors = 17
 approx_num_connections = 10
 defaulted_prob = 0.01
 
-nodes = []
-for _ in range(num_nodes):
-    node = Node()
-    node.set_feature('defaulted', np.random.choice([0, 1], p=[1 - defaulted_prob, defaulted_prob]))
-    node.set_feature('sector', np.random.randint(num_sectors))
-    nodes.append(node)
+graph = nx.DiGraph()
 
-nodes = np.array(nodes, dtype=Node)
+all_node_ids = range(num_nodes)
 
-graph = Graph(nodes)
+for node_id in all_node_ids:
+    num_connections = np.clip(np.random.poisson(approx_num_connections), 1, num_nodes - 1)
+    accepted_node_ids = list(filter(lambda idx: idx != node_id, all_node_ids))  # avoid self-looping
+    connected_node_ids = np.random.choice(accepted_node_ids, num_connections, replace=False)
 
-for node_id in range(graph.num_nodes):
-    num_connections = min(np.random.poisson(approx_num_connections), graph.num_nodes - 1)
-    connections = np.random.choice(range(graph.num_nodes), num_connections, replace=False)
-    graph.set_connections(node_id, connections)
-
-    node = nodes[node_id]
-    node.set_feature('all_connected', connections)
     weights_val = 1 / num_connections
-    node.set_feature('weights', weights_val * np.ones(num_connections))
+    weighted_edges = [(node_id, to_node_id, weights_val) for to_node_id in connected_node_ids]
 
-graph.save('graphs/test.pickle')
+    node_attrs = {
+        'id': node_id,
+        'defaulted': np.random.choice([0., 1.], p=[1 - defaulted_prob, defaulted_prob]),
+        'sector': np.random.randint(num_sectors),
+        'all_connected_nodes': connected_node_ids
+    }
+
+    graph.add_node(node_id, **node_attrs)
+    graph.add_weighted_edges_from(weighted_edges)
+
+pickle.dump(graph, open('graphs/new.pickle', 'wb'))
--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-import numpy as np
-from nptyping import Array
-from Graph import Graph
-from Node import Node
+import pickle
 
-import matplotlib.pyplot as plt
 import matplotlib.animation as anim
+import matplotlib.pyplot as plt
+import numpy as np
+from networkx import DiGraph
 
 # Parameters, modifiable
 iterations = 100
@@ -12,19 +11,18 @@ beta = 0.5
 mu = 0.4
 
 # Load in graph
-graph = Graph.load('graphs/test.pickle')
+graph = pickle.load(open('graphs/new.pickle', 'rb'))
 
 
 # Algorithm
 
-
-def get_nodes_per_sector(graph):
+def get_nodes_per_sector(graph: DiGraph):
     nodes_per_sector = {}
 
-    for node_id in range(graph.num_nodes):
-        node = graph.get_nodes(node_id)
+    for node_id in graph:
+        node = graph.nodes[node_id]
 
-        sector = node.get_feature('sector')
+        sector = node['sector']
         if sector in nodes_per_sector:
             nodes_per_sector[sector] = np.append(nodes_per_sector[sector], node_id)
         else:
@@ -33,64 +31,68 @@ def get_nodes_per_sector(graph):
     return nodes_per_sector
 
 
-def calculate_q(nodes: Array[Node], r):
+def calculate_q(nodes, weights):
     defaulted_probs = []
     for node in nodes:
-        defaulted_probs.append(node.get_feature('defaulted'))
+        defaulted_probs.append(node['defaulted'])
 
     defaulted_probs = np.array(defaulted_probs)
-    return np.prod(1 - beta * r * defaulted_probs)
+    return np.prod(1 - beta * weights * defaulted_probs)
 
 
 defaulted_density = []
 nodes_per_sector = get_nodes_per_sector(graph)
 
 
-def update_default(node: Node, graph: Graph):
-    defaulted_p = node.get_feature('defaulted')
-    r = node.get_feature('weights')
-    connected_nodes = graph.get_connected_nodes(node.id)
+def update_default(node, graph: DiGraph):
+    defaulted_p = node['defaulted']
+    neighbors = graph[node['id']]
 
-    q = calculate_q(connected_nodes, r)
+    weights = np.array([neighbor['weight'] for neighbor in neighbors.values()])
+    neighbor_nodes = [graph.nodes[node_id] for node_id in list(neighbors)]
+
+    q = calculate_q(neighbor_nodes, weights)
     new_defaulted_p = (1 - q) * (1 - defaulted_p) + (1 - mu) * defaulted_p + mu * (1 - q) * defaulted_p
     # new_defaulted_p = np.clip(new_defaulted_p, 0, 1)
     # new_defaulted_p = defaulted_p if 0 > new_defaulted_p > 1 else new_defaulted_p
     new_defaulted_p = 1 if np.random.random() > new_defaulted_p else 0
 
-    all_connected_nodes = node.get_feature('all_connected')
+    all_connected_nodes = node['all_connected_nodes']
 
-    for connected_node in connected_nodes:
-        if connected_node.get_feature('defaulted') > defaulted_p:
-            graph.remove_connections(node.id, connected_node.id)
-            sector = connected_node.get_feature('sector')
+    for weight, connected_node in zip(weights, neighbor_nodes):
+        if connected_node['defaulted'] > defaulted_p:
+            graph.remove_edge(node['id'], connected_node['id'])
+            sector = connected_node['sector']
             nodes_in_sector = nodes_per_sector[sector]
             not_in_connected_mask = np.isin(nodes_in_sector, all_connected_nodes, invert=True)
-            eligible_nodes = graph.get_nodes(nodes_in_sector[not_in_connected_mask])
-            eligible_nodes = np.array([node for node in eligible_nodes if node.get_feature('defaulted') <= defaulted_p])
+            eligible_nodes = [graph.nodes[node_id] for node_id in nodes_in_sector[not_in_connected_mask]]
+            eligible_nodes = np.array([node for node in eligible_nodes if node['defaulted'] <= defaulted_p])
 
             if eligible_nodes.size == 0:
-                new_weights = node.get_feature('weights')[:-1]
-                new_weights /= np.sum(new_weights)
-                node.set_feature('weights', new_weights)
+                weights = [neighbor['weight'] for neighbor in neighbors.values()]
+                sum_weights = np.sum(weights)
+                updated_edges = [(node['id'], to_node_id, val['weight'] / sum_weights) for to_node_id, val in
+                                 neighbors.items()]
+                graph.update(edges=updated_edges)
                 continue
 
             new_node = np.random.choice(eligible_nodes)
-            graph.set_connections(node.id, new_node.id)
-            all_connected_nodes = np.append(all_connected_nodes, new_node.id)
-            node.set_feature('all_connected', all_connected_nodes)
+            graph.add_edge(node['id'], new_node['id'], weight=weight)
+            all_connected_nodes = np.append(all_connected_nodes, new_node['id'])
+            node['all_connected'] = all_connected_nodes
 
-    node.set_feature('defaulted', new_defaulted_p)
+    node['defaulted'] = new_defaulted_p
 
 
-def iteration(graph: Graph):
+def iteration(graph: DiGraph):
     total_p = 0
 
-    for node_id in range(graph.num_nodes):
-        node = graph.get_nodes(node_id)
-        total_p += node.get_feature('defaulted')
+    for node_id in graph:
+        node = graph.nodes[node_id]
+        total_p += node['defaulted']
         update_default(node, graph)
 
-    cur_density = total_p / graph.num_nodes
+    cur_density = total_p / graph.number_of_nodes()
     defaulted_density.append(cur_density)
 
 
