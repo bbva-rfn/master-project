8b91f887811f1c383c837ff34fd7f5ed04ecea19
Ramon <ramon4mm@gmail.com>
2019-04-03 13:00:06 +0200

    changes in plots
    added cascading plot although for just a 1000 node graph does not yield anything beautiful. Also made the betas plot a function.

--- analyze.py
+++ analyze.py
@@ -2,7 +2,8 @@ import pickle
 import matplotlib.pyplot as plt
 import networkx
 from SecNet import SecNet
-from cascades import cascade_fake_origins, check_cascade_size,check_cascade_size_recursive
+from cascades import cascade_fake_origins,check_cascade_size_recursive
+from plots_sis import cascade_size_plot
 
 sn = pickle.load(open('results/test.pickle', 'rb'))
 graph = sn.graph
@@ -12,6 +13,9 @@ cascade_size = check_cascade_size_recursive(graph, origins)
 #cascade_size = check_cascade_size(graph, origins)
 print(cascade_size)
 
+n = graph.number_of_nodes()
+
+cascade_size_plot(cascade_size,n)
 
 # plt.figure(figsize=(12, 12))
 # networkx.draw_spectral(graph, with_labels=False, node_size=10)
--- plots_sis.py
+++ plots_sis.py
@@ -5,6 +5,7 @@ import pickle
 import numpy as np
 import matplotlib.pyplot as plt
 from SecNet import SecNet
+from networkx import DiGraph
 
 # Load in graph + parameters
 def construct_probs_by_sector(density_probs):
@@ -16,39 +17,69 @@ def construct_probs_by_sector(density_probs):
             p.append(prob[i])
         probs.append(p)
     return probs
-        
-mu = 0.4
-betas = np.arange(0,1,0.1)
-iterations = 50
 
-density_probs = []
-g = pickle.load(open('graphs/new.pickle', 'rb'))
-
-for beta in betas:
-    sn = SecNet(g, mu, beta)
-    sn.run(iterations)
-    #sn.graph to access the copied graph
-    prob_by_sector = np.zeros(17)
-
-    for sector in range(0,17):
-        nodes = sn.nodes_per_sector[sector]
-        n = len(nodes)
-        for node_id in nodes:
-            node = sn.graph.nodes[node_id]
-            prob_by_sector[sector] += node['defaulted']
-        prob_by_sector[sector] = prob_by_sector[sector]/n
+def beta_plot(g:DiGraph,mu=0.4,iterations = 75):        
+    betas = np.arange(0,1,0.1)
     
-    density_probs.append(prob_by_sector)
+    density_probs = []
+    
+    for beta in betas:
+        sn = SecNet(g, mu, beta)
+        sn.run(iterations)
+        #sn.graph to access the copied graph
+        prob_by_sector = np.zeros(17)
+    
+        for sector in range(0,17):
+            nodes = sn.nodes_per_sector[sector]
+            n = len(nodes)
+            for node_id in nodes:
+                node = sn.graph.nodes[node_id]
+                prob_by_sector[sector] += node['defaulted']
+            prob_by_sector[sector] = prob_by_sector[sector]/n
+        
+        density_probs.append(prob_by_sector)
+    
+    print(density_probs)
+    
+    probs_by_sector = construct_probs_by_sector(density_probs)
+    print(probs_by_sector)
+    plt.figure() 
+    for prob in probs_by_sector:
+        plt.plot(betas,prob)
+    plt.xlabel('betas')
+    plt.ylabel('density prob')
+    plt.savefig('images/prob_by_sector.png')
+    plt.show()
+    
+    
+def cascade_size_plot(sizes,n): #n is the number of nodes, if graph is not passed then we need it as argument
+    if type(sizes[0]) == int and len(sizes)>1: #meaning there is only one list 
+        max_sizes = max(sizes)
+        prob = []
+        for i in range(0,max_sizes+1):
+            p = 0
+            for j in range(0,len(sizes)):
+                if(i ==sizes[j]):
+                    p+=1
+            p = p/n
+            prob.append(p)
+        #now we have a list of probabilities and we need to do cumulative distribution
+        inv_cum = 1-np.cumsum(prob)
+        
+        plt.figure()
+        plt.xlabel('cs')
+        plt.ylabel('1-P(cs<Cs)')
+        plt.xscale('log')
+        plt.yscale('log')
+        plt.plot(np.arange(0,max_sizes+1),inv_cum)
+        plt.savefig('images/cascade_size.png')
+        plt.show()
+    else:
+        print('Not implemented yet for larger sizes')
+            
+        
+        
+    
 
-print(density_probs)
 
-probs_by_sector = construct_probs_by_sector(density_probs)
-print(probs_by_sector)
-plt.figure() 
-for prob in probs_by_sector:
-    plt.plot(betas,prob)
-plt.xlabel('betas')
-plt.ylabel('density prob')
-plt.savefig('images/prob_by_sector.png')
-plt.show()
 
