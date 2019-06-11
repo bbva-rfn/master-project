3d236f13c2211c24e86d53dfe0be48e80f0b858c
Ramon <ramon4mm@gmail.com>
2019-06-11 13:39:58 +0200

    new risk functions 1 vs 1
    They analyses the risk of one specific node becoming defaulted to another specific node. In this way we avoid the larger execution time in case we want to analyze just this case.

--- risk.py
+++ risk.py
@@ -1,10 +1,21 @@
 # -*- coding: utf-8 -*-
 # risk functions implementation
 
-from risk_functions import *
+from risk_functions import risk_for_me_one,risk_for_me_one_strict
 import pickle
 
 g = pickle.load(open('graphs/new.pickle', 'rb'))
-risk_for_me(g,4)
+'''
+with nodes 4,7 I obtain risk 1 in both, curious 
 
-print('Yeah!')
+we try now 4,117 and also risk 1 as it says first defaulted at 0
+Problem!
+Solved the problem
+now it gives 0 for lax risk and 0.5 for strict which is nice for 2 repetitions a study 
+should be performmed
+'''
+
+risk = risk_for_me_one(g,4,117,repetitions=2)
+print("Lax risk",risk)
+risk = risk_for_me_one_strict(g,4,117,repetitions=2)
+print("Strict risk",risk)
--- risk_functions.py
+++ risk_functions.py
@@ -2,18 +2,18 @@
 
 from networkx import DiGraph
 from SecNet import SecNet, ReconnectionPolicy
-
+import numpy as np
 
 def set_initial_defaults(graph:DiGraph,node_id):
-    for node_id in graph:
-        node = graph.nodes[node_id]
+    for node_i in graph:
+        node = graph.nodes[node_i]
         if node['id'] == node_id:
             node['defaulted'] = 1
         else:
             node['defaulted'] = 0
 
     
-def risk_for_me(graph:DiGraph,node_id,iterations=70,mu=0.2,beta=0.6,delay=2,weight_transfer=False):
+def risk_for_me_all(graph:DiGraph,node_id,iterations=70,mu=0.2,beta=0.6,delay=2,weight_transfer=False):
     risks = []
     for node_i in graph:
         node = graph.nodes[node_i]
@@ -34,6 +34,45 @@ def risk_for_me(graph:DiGraph,node_id,iterations=70,mu=0.2,beta=0.6,delay=2,weig
         else:
             lis[1] = 1.-lis[1]/100.
     return risks
+
+def risk_for_me_one(graph:DiGraph,node_id_me,node_id_other,repetitions=20,iterations=100,
+                    mu=0.2,beta=0.6,delay=2,weight_transfer=False):
+    risks = []
+    for i in range(repetitions):
+        set_initial_defaults(graph,node_id_other)
+        sn = SecNet(graph, mu, beta, reconnection_policy=ReconnectionPolicy.RANDOM,
+                    default_delay=delay, weight_transfer=weight_transfer)
+        sn.run(iterations)
+        risk = sn.graph.nodes[node_id_me]['first_defaulted_at']
+        #print(risk)
+        #print(sn.graph.nodes[node_id_me])
+        '''for node_id in sn.graph.nodes:
+            node = sn.graph.nodes[node_id]
+            print(node['first_defaulted_at'])
+         '''   
+        if risk == -1:
+            risk = 0
+        else:
+            risk = 1.-float(risk/iterations)
+        risks.append(risk)
+    return np.mean(risks)
+
+def risk_for_me_one_strict(graph:DiGraph,node_id_me,node_id_other,repetitions=20,
+                           iterations=100,mu=0.2,beta=0.6,delay=2,weight_transfer=False):
+    
+    risks = []
+    for i in range(repetitions):
+        set_initial_defaults(graph,node_id_other)
+        sn = SecNet(graph, mu, beta, reconnection_policy=ReconnectionPolicy.RANDOM,
+                    default_delay=delay, weight_transfer=weight_transfer)
+        sn.run(iterations)
+        risk = sn.graph.nodes[node_id_me]['first_defaulted_at']
+        if risk == -1:
+            risk = 0
+        else:
+            risk = 1
+        risks.append(risk)
+    return np.mean(risks)
 
 def risk_overall(graph:DiGraph,repetitions=10,iterations=75,mu=0.2,beta=0.6,delay=2,weight_transfer=False):
     #we need an atribute that is risk_overall 
