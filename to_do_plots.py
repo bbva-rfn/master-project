5068d56599695d0a29778d74a0ed243587bb84bb
sergi sanchez de la blanca contreras <sergi@MacBook-Air-de-sergi-2.local>
2019-06-12 18:14:55 +0200

    replicate_density and pct_density
    Create a function to replicate the experiment n times, and then calculate the pct

--- to_do_plots.py
+++ to_do_plots.py
@@ -18,10 +18,29 @@ density_plot(g,
              iterations = 200)
          
 '''
-sn = SecNet(g, mu = 0.2, beta = 0.6, 
-                                reconnection_policy = ReconnectionPolicy.RANDOM, 
-                                default_delay = 2  , weight_transfer = False)
-sn.run(150,variation_coeff = 10e-5)
-print(sn.defaulted_density)
-print(np.mean(sn.defaulted_density[-10:]))
-sn.plot()
+def replicate_density (niter = 100, 
+                       beta = 0.6):
+    dens = []
+    for i in range (niter):
+        sn = SecNet(g, mu = 0.2, 
+                    beta = beta, 
+                    reconnection_policy = ReconnectionPolicy.SOFT, 
+                    default_delay = 2  , 
+                    weight_transfer = False)
+        sn.run(400,variation_coeff = 10e-5)
+        sn.plot()
+        dens.append( sn.defaulted_density[- 1] )
+        print(i)
+        return (dens)
+
+replicate_density(niter 100, beta = 0.5 )
+
+def pct_density(dens):
+    count = 0
+    for i in range(100):
+        if (np.array(dens[i]) > 0.1):
+            count = count + 1       
+    print(count/100)
+
+
+replicate_density(dens)
