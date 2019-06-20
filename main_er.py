f29c9108180b47896ace98b5c5601b318124289a
Ramon <ramon4mm@gmail.com>
2019-06-20 18:02:51 +0200

    ER results
    Surprisingly comunism works better!

--- main_er.py
+++ main_er.py
@@ -10,12 +10,11 @@ import time
 
 start = time.time()
 g = pickle.load(open('ER/graph_er.pickle', 'rb'))
-
+'''
 sizes = cascades_sizes_multiple(g,mu=0.2,beta=0.6,delays=[2,3,4,5],policy='SOFT',repetitions=30,
                                 filename='ER/results/cascades_ratio3')
 
 #sizes = pickle.load(open('BA/results/cascade_sizeSOFT[2, 3, 4, 5].pickle','rb'))
-
 end = time.time()
 
 print(end-start)
@@ -25,14 +24,14 @@ plot_cascade_sizes(sizes,delays=[2,3,4,5],ylim = 10e-4,colors=['r','g','b','k'],
 
 
 start = time.time()
-
+'''
 densities = compare_density(g,mu = 0.2,beta=0.4,repetitions=8,max_iterations=150,policy = 'SOFT',
-                            delays=[1,3,5,7],filename='ER/results/comparison_ratio2_')
+                            delays=[9,10,11],filename='ER/results/comparison_ratio2_')
 end = time.time()
 print(end - start)
 
-plot_comparison_densities(densities,delays=[1,3,5,7],
-                          filename='ER/images/comparison_ratio2_SOFT.png')
+plot_comparison_densities(densities,delays=[9,10,11],
+                          filename='ER/images/comparison3_ratio2_SOFT.png')
 
 
 '''
