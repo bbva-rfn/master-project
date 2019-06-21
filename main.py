e106c7f4ba176eaa8da33ef08c0a89f9d6ba4f92
Ramon <ramon4mm@gmail.com>
2019-06-21 19:53:29 +0200

    more plots
    who would have guessed right?

--- main.py
+++ main.py
@@ -11,11 +11,12 @@ from sectorial_density_functions import density_with_sigma,plot_density_sigma
 g = pickle.load(open('ER/graph_er.pickle', 'rb'))
 start = time.time()
 
-name_or = 'ER/results/density_ratio2_SOFT'
-for delay in [5,6,7,8]:
+name_or = 'ER/results/density_ratio3_RANDOM'
+for delay in [2,3]:
     name = name_or+str(delay)+'.pickle'
-    name2 = 'ER/images/density_ratio2_SOFT'+str(delay)+'.png'
-    densities = density_with_sigma(g,beta=0.4,delay=delay,repetitions=100,filename=name)
+    name2 = 'ER/images/density_ratio3_RANDOM'+str(delay)+'.png'
+    densities = density_with_sigma(g,beta=0.6,delay=delay,repetitions=100,
+                                   policy=ReconnectionPolicy.RANDOM,filename=name)
     d_aux = densities[densities['Iteration']==140]['Density']>0.35
     print(d_aux.sum()/len(d_aux))
     d_aux = densities[densities['Iteration']==140]['Density']<0.05
