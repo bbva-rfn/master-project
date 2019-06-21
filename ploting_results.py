98f77bdcdff598b2fe5c62d919a737ef967a0761
Ramon <ramon4mm@gmail.com>
2019-06-21 19:17:22 +0200

    More plots
    soon, more plots

--- ploting_results.py
+++ ploting_results.py
@@ -3,10 +3,9 @@ import pickle
 import time
 from sis_delay_comparison import plot_comparison_densities
 
-d1 = pickle.load(open('ER/results/comparison2_ratio2_RANDOM[3, 5, 7].pickle', 'rb'))
-d2 = pickle.load(open('ER/results/comparison2_ratio2_RANDOM[2, 4, 8].pickle', 'rb'))
-d3 = pickle.load(open('ER/results/comparison2_ratio2_RANDOM[9, 10, 11].pickle', 'rb'))
-d4 = pickle.load(open('ER/results/comparison2_ratio2_RANDOM[1, 6].pickle', 'rb'))
+d1 = pickle.load(open('ER/results/comparison_ratio2_SOFT[1, 3, 5, 7].pickle', 'rb'))
+d2 = pickle.load(open('ER/results/comparison_ratio2_SOFT[2, 4, 6, 8].pickle', 'rb'))
+d3 = pickle.load(open('ER/results/comparison_ratio2_SOFT[9, 10, 11].pickle', 'rb'))
 start = time.time()
 
 for d in d2:
@@ -15,9 +14,7 @@ for d in d2:
 for d in d3:
     d1.append(d)
 
-for d in d4:
-    d1.append(d)
-plot_comparison_densities(d1, title='RANDOM policy', delays=[3, 5, 7, 2, 4, 8, 9, 10, 11,1],
-                          filename='ER/images/delay_comparison_RANDOM2.png')
+plot_comparison_densities(d1, title='SOFT policy', delays=[1, 3, 5, 7, 2, 4, 6, 8, 9, 10, 11],
+                          filename='ER/images/delay_comparison2_SOFT2.png')
 
 end = time.time()
