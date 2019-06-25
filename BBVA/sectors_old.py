60b5d0189107fd42c8f02cbe8144419e5b80c009
Philippe van Amerongen <philippe.vanamerongen@gmail.com>
2019-06-25 11:18:44 +0200

    Change BBVA sector names

--- BBVA/sectors_old.py
+++ BBVA/sectors_old.py
@@ -1,121 +1,104 @@
 sectors = {
-    0: {
+    'Financial Institutions': {
         'size': 0.00046,
         'in': 39.613,
         'out': 45.529,
         'default': 0.03650,
-        'name': 'Financial Institutions'
     },
-    1: {
+    'Energy': {
         'size': 0.00083,
         'in': 12.844,
         'out': 8.666,
         'default': 0.01111,
-        'name': 'Energy'
     },
-    2: {
+    'Financial Services': {
         'size': 0.01165,
         'in': 6.300,
         'out': 20.265,
         'default': 0.00786,
-        'name': 'Financial Services'
     },
-    3: {
+    'Utilities': {
         'size': 0.01529,
         'in': 5.589,
         'out': 5.903,
         'default': 0.01264,
-        'name': 'Utilities'
     },
-    4: {
+    'Telecom': {
         'size': 0.03299,
         'in': 5.960,
         'out': 5.194,
         'default': 0.01776,
-        'name': 'Telecom'
     },
-    5: {
+    'Basic Materials': {
         'size': 0.02745,
         'in': 5.789,
         'out': 5.350,
         'default': 0.02782,
-        'name': 'Basic Materials'
     },
-    6: {
+    'Transportation': {
         'size': 0.04064,
         'in': 5.411,
         'out': 4.336,
         'default': 0.01868,
-        'name': 'Transportation'
     },
-    7: {
+    'Retail': {
         'size': 0.23593,
         'in': 3.973,
         'out': 3.233,
         'default': 0.01217,
-        'name': 'Retail'
     },
-    8: {
+    'Retailers': {
         'size': 0.04273,
         'in': 5.001,
         'out': 3.613,
         'default': 0.01885,
-        'name': 'Retailers'
     },
-    9: {
+    'Capital Goods': {
         'size': 0.08689,
         'in': 4.528,
         'out': 3.098,
         'default': 0.01866,
-        'name': 'Capital Goods'
     },
-    10: {
+    'Auto': {
         'size': 0.0147,
         'in': 4.454,
         'out': 2.991,
         'default': 0.01786,
-        'name': 'Auto'
     },
-    11: {
+    'Consumer and Healthcare': {
         'size': 0.07055,
         'in': 3.259,
         'out': 3.770,
         'default': 0.01539,
-        'name': 'Consumer and Healthcare'
     },
-    12: {
+    'Construction and Infrastructure': {
         'size': 0.08907,
         'in': 3.067,
         'out': 3.270,
         'default': 0.03071,
-        'name': 'Construction and Infrastructure'
     },
-    13: {
+    'Unknown': {
         'size': 0.10159,
         'in': 0.930,
         'out': 1.413,
         'default': 0.01942,
-        'name': 'Unknown'
     },
-    14: {
+    'Real Estate': {
         'size': 0.06843,
         'in': 1.517,
         'out': 1.833,
         'default': 0.03603,
-        'name': 'Real Estate'
     },
-    15: {
+    'Leisure': {
         'size': 0.12861,
         'in': 2.509,
         'out': 2.512,
         'default': 0.01511,
-        'name': 'Leisure'
     },
-    16: {
+    'Institutions': {
         'size': 0.03219,
         'in': 5.547,
         'out': 10.764,
         'default': 0.0535,
-        'name': 'Institutions'
     }
 }
