2216153bc9cb72f697fdaab01485ef61b7894ce7
sergi sanchez de la blanca contreras <sergi@MacBook-Air-de-sergi-2.local>
2019-06-25 17:12:35 +0200

    cascades

--- main.py
+++ main.py
@@ -16,14 +16,14 @@ d_aux = densities[densities['Iteration']==145]['Density']<0.05
 print(d_aux.sum()/len(d_aux))
 
 '''
-g = pickle.load(open('ER/graph_er.pickle', 'rb'))
+g = pickle.load(open('BA/graph_ba.pickle', 'rb'))
 
 start = time.time()
 
-name_or = 'ER/results/density_control.pickle'
-name2 = 'ER/images/density_control.png'
+name_or = 'BA/results/density_control3.pickle'
+name2 = 'BA/images/density_control3.png'
 
-densities = density_with_sigma(g,beta=0.4,delay=1,repetitions=100,
+densities = density_with_sigma(g,beta=0.6,delay=1,repetitions=100,
                                    policy=ReconnectionPolicy.NONE,filename=name_or)
 d_aux = densities[densities['Iteration']==140]['Density']>0.35
 print(d_aux.sum()/len(d_aux))
