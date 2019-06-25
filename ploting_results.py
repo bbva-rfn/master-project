de008d05e199ad1b3f42f00fd2d9462d86c0e82c
Ramon <ramon4mm@gmail.com>
2019-06-25 14:28:50 +0200

    minor changes + one cascade for random

--- ploting_results.py
+++ ploting_results.py
@@ -2,6 +2,7 @@ import pickle
 from plots_sis import plot_sectorial_multi_beta
 import numpy as np
 
+'''
 probs = pickle.load(open('BBVA/results/density_sectorialSOFT2.pickle', 'rb'))
 
 names = {'0':'Stark' , '1':'Bolton', '2':'Umber','3':'Manderly','4':'Hornwood','5':'Glover',
@@ -10,3 +11,6 @@ names = {'0':'Stark' , '1':'Bolton', '2':'Umber','3':'Manderly','4':'Hornwood','
 
 betas = np.arange(0,1,0.1)
 plot_sectorial_multi_beta(probs,names,betas,filename='BBVA/images/sectorial_density1.png')
+'''
+
+probs = pickle.load(open('BBVA/results/density_sectorialSOFT2.pickle', 'rb'))
