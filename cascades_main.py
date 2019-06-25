e4c62820549cdc14c5e4683134cb478fb08a78d5
Ramon <ramon4mm@gmail.com>
2019-06-25 12:22:43 +0200

    new plots
    To make all the density curves delay comparison

--- cascades_main.py
+++ cascades_main.py
@@ -5,7 +5,7 @@ import time
 start = time.time()
 g = pickle.load(open('BA/graph_ba.pickle', 'rb'))
 
-sizes = cascades_sizes_multiple(g,mu=0.2,beta=0.4,delays=[3,4,5],policy='SOFT',repetitions=20,
+sizes = cascades_sizes_multiple(g,mu=0.2,beta=0.4,delays=[6,7,8],policy='SOFT',repetitions=20,
                                 filename='BA/results/cascades/')
 
 
@@ -16,4 +16,4 @@ print(end-start)
 print(sizes)
 
 plot_cascade_sizes(sizes,delays=[3,4,5],ylim = 10e-4,colors=['r','g','b','k'],
-                   filename='BA/images/cascades/compa2_SOFT.png')
+                   filename='BA/images/cascades/compa3_SOFT.png')
--- ploting_results.py
+++ ploting_results.py
@@ -3,9 +3,9 @@ import pickle
 import time
 from sis_delay_comparison import plot_comparison_densities
 
-d1 = pickle.load(open('ER/results/comparison_ratio3_RANDOM[1, 3, 5].pickle', 'rb'))
-d2 = pickle.load(open('ER/results/comparison_ratio3_RANDOM[2, 4, 6].pickle', 'rb'))
-#d3 = pickle.load(open('ER/results/comparison_ratio3_SOFT[9, 10, 11].pickle', 'rb'))
+d1 = pickle.load(open('BA/results/comparison_ratio3_RANDOM[1, 3, 5].pickle', 'rb'))
+d2 = pickle.load(open('BA/results/comparison_ratio3_RANDOM[2, 4, 6].pickle', 'rb'))
+#d3 = pickle.load(open('BA/results/comparison_ratio2_RANDOM[9, 10, 11].pickle', 'rb'))
 start = time.time()
 
 for d in d2:
@@ -14,7 +14,7 @@ for d in d2:
 for d in d3:
     d1.append(d)
 '''
-plot_comparison_densities(d1, title='RANDOM policy', delays=[1, 3, 5, 2, 4, 6],
-                          filename='ER/images/delay_comparison2_RANDOM3.png')
+plot_comparison_densities(d1, title='RANDOM policy', delays=[1, 2, 3, 4, 5, 6],
+                          filename='BA/images/delay_comparison_RANDOM3.png')
 
 end = time.time()
