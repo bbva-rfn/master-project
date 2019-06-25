342b7015f92b063422078f154403739f6a94d63a
Ramon <ramon4mm@gmail.com>
2019-06-25 12:35:13 +0200

    cascade deletions/additions

--- cascades_main.py
+++ cascades_main.py
@@ -3,10 +3,10 @@ import pickle
 import time
 
 start = time.time()
-g = pickle.load(open('BA/graph_ba.pickle', 'rb'))
+g = pickle.load(open('ER/graph_er.pickle', 'rb'))
 
-sizes = cascades_sizes_multiple(g,mu=0.2,beta=0.4,delays=[6,7,8],policy='SOFT',repetitions=20,
-                                filename='BA/results/cascades/')
+sizes = cascades_sizes_multiple(g,mu=0.2,beta=0.4,delays=[6,7,8,9],policy='SOFT',repetitions=20,
+                                filename='ER/results/cascades/')
 
 
 end = time.time()
@@ -15,5 +15,5 @@ print(end-start)
 
 print(sizes)
 
-plot_cascade_sizes(sizes,delays=[3,4,5],ylim = 10e-4,colors=['r','g','b','k'],
-                   filename='BA/images/cascades/compa3_SOFT.png')
+plot_cascade_sizes(sizes,delays=[6,7,8,9],ylim = 10e-4,colors=['r','g','b','k'],
+                   filename='ER/images/cascades/compa_SOFT2.png')
