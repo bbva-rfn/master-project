b6447c7f4f0ea920e6755797af737a3c76ee182e
P.G.J van Amerongen <philippe.vanamerongen@gmail.com>
2019-06-26 16:31:53 +0200

    BBVA cascades_ratio2_SOFT[5, 6]

--- cascades_main.py
+++ cascades_main.py
@@ -3,20 +3,18 @@ import pickle
 import time
 
 start = time.time()
-g = pickle.load(open('BA/graph_ba.pickle', 'rb'))
+g = pickle.load(open('BBVA/bbva.pickle', 'rb'))
 
-
-
-sizes = cascades_sizes_multiple(g,mu=0.2,beta=0.6,delays=[1,9],policy='SOFT',repetitions=30,
-                                filename='BA/results/cascades/')
+sizes = cascades_sizes_multiple(g,mu=0.2,beta=0.4,delays=[5,6],policy='SOFT',repetitions=50,
+				filename='BBVA/results/cascades_')
 
 end = time.time()
 
 print(end-start)
 
-print(sizes)
+#print(sizes)
 
-res = plot_cascade_sizes(sizes,delays=[1,9],ylim = 10e-4,
-                         filename='BA/images/cascades/compa6_SOFT3.png')
+#res = plot_cascade_sizes(sizes,delays=[1,9],ylim = 10e-4,
+#                         filename='BA/images/cascades/compa6_SOFT3.png')
 
-print(res)
+#print(res)
