43d73246a84578bc565e95cf0dc9cd0fe07d7b88
Philippe van Amerongen <philippe.vanamerongen@gmail.com>
2019-06-26 14:42:33 +0200

    Add all connected in attribute to all graphs

--- add_attribute.py
+++ add_attribute.py
@@ -1,10 +1,12 @@
 import pickle
 
-g = pickle.load(open('BBVA/bbva.pickle', 'rb'))
+name = 'BBVA/bbva'
+
+g = pickle.load(open(name + '.pickle', 'rb'))
 
 for node_id in g:
     in_conn = [edge[0] for edge in g.in_edges(node_id)]
     node = g.nodes[node_id]
     node['all_connected_nodes_in'] = in_conn
 
-pickle.dump(g, open('BBVA/bbva.pickle', 'wb'))
+pickle.dump(g, open(name + '.pickle', 'wb'))
